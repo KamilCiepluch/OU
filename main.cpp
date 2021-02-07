99dc8f0f8db4795e949e3f1e6e6c7d074c545525
Kamil <235845@eu.p.lodz.pl>
2021-02-07 15:29:55 +0100

    branch 01

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int Policz (int a, int b) {
     return a+b;
 }
 int main() {
+    std::cout << "Hello, World!" << std::endl;
     cout <<"W pierwszym branchu; 2+3=" << Policz(2,3);
     return 0;
 }
