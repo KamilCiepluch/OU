4b30d5f5e3f0cb0f25fddef8304ccd5142e40dbd
Kamil <235845@eu.p.lodz.pl>
2021-02-07 14:58:28 +0100

    initial commit

---
+++ cmake-build-debug/CMakeFiles/OU.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU.dir/main.cpp.obj"
+  "OU.exe"
+  "OU.exe.manifest"
+  "OU.pdb"
+  "libOU.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OU.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
